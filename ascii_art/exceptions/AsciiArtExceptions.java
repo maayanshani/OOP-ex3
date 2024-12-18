9ea879bc35f7000bf42c94777c110e6125734856
rotem <rotem.garti@mail.huji.ac.il>
2024-12-18 20:55:34 +0200

    exit, chars, add, remove, res, output, asciiArt - DONE
    with exceptions

--- ascii_art/exceptions/AsciiArtExceptions.java
+++ ascii_art/exceptions/AsciiArtExceptions.java
@@ -8,6 +8,8 @@ package ascii_art.exceptions;
  *          V general (command not in the 8) - INVALID_COMMAND_MESSAGE
  *          V output (incorrect format) - OUTPUT_INCORRECT_FORMAT_MESSAGE
  *          V res (out of range, incorrect format) - INVALID_RES_MESSAGE, RES_INCORRECT_FORMAT_MESSAGE
+ *          V add and remove (incorrect format) - ADD_INVALID_COMMAND_MESSAGE, REMOVE_INVALID_COMMAND_MESSAGE
+ *          - round
 *       V output (opening file) - OUTPUT_FAILED_MESSAGE, exit the program
  * - ? BrightnessMatrix (extends AsciiAlgo):
  *      - if got null Image[][] (can be the same as ImageProcessor)
